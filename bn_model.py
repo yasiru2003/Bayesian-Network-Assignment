8467ef1ccc90a2b27ce3f088c7a0a35a74473046
Yasiru Bandara <bandarakgpy.23@uom.lk>
2025-12-26 18:35:54 +0530

    Update bn_model.py

--- bn_model.py
+++ bn_model.py
@@ -7,7 +7,7 @@ from pgmpy.inference import VariableElimination
 # loding the data from local csv file
 data = pd.read_csv("lung_cancer.csv")
 
-# changin yes/no to 1 and 0
+
 data['LUNG_CANCER'] = data['LUNG_CANCER'].map({'YES': 1, 'NO': 0})
 
 # fix other colums too
@@ -34,23 +34,30 @@ model = DiscreteBayesianNetwork([
 # lerning parameters from the data
 model.fit(data, estimator=MaximumLikelihoodEstimator)
 
-# doing inferense now
+
+calculate probabilities by summing out other variables.
 infer = VariableElimination(model)
 
-# high risk case
+# Case 1:- High risk (smoker and pollution)
 res1 = infer.query(variables=['Lung_Disease'], evidence={'Smoking': 1, 'Pollution': 1})
 print("\nHigh Risk (Smoker=Yes, Pollution=Yes):")
 print(res1)
 
-# low risk case
+# Case 2:- Low risk (non smoker and low pollution)
 res2 = infer.query(variables=['Lung_Disease'], evidence={'Smoking': 0, 'Pollution': 0})
 print("\nLow Risk (Smoker=No, Pollution=No):")
 print(res2)
 
+# Case 3:- Using symptoms (Technical Depth improvement) :- What is the probability of disease if someone has a cough and chest pain?
+res3 = infer.query(variables=['Lung_Disease'], evidence={'Cough': 1, 'Chest_Pain': 1})
+print("\nProbability with Symptoms (Cough=Yes, Chest Pain=Yes):")
+print(res3)
+
 # saving cpts for report
 with open("cpts.txt", "w") as f:
+    f.write("Methodology: Using pgmpy library with Variable Elimination algorithm\n\n")
     for cpd in model.get_cpds():
         f.write(f"CPD for {cpd.variable}:\n")
         f.write(str(cpd) + "\n\n")
 
-print("Process completed")
+print("Process completed.")
